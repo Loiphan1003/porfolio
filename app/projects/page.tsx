9ff432cbe1484e555e054ea13ba081151e2061fe
Loiphan1003 <phanvuloi001@gmail.com>
2023-07-12 15:55:18 +0700

    add responsive for projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const Projects = () => {
   return (
     <main
-        className="mt-[48px]"
+        className="mt-[2rem] md:mt-[48px]"
     >
         <header>
-            <h1 className="text-[48px] font-bold not-italic leading-[53px]" >Projects</h1>
+            <h1 className="text-[2.2rem] md:text-[48px] font-bold not-italic leading-[53px]" >Projects</h1>
         </header>
 
         <section
-            className="mt-[48px] grid gap-4 grid-cols-3 md:grid-cols-2"
+            className="mt-[48px] grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
         >
             <ProjectItem />
             <ProjectItem />
