c82c0c53b6c7f44414529b6b69faa4f4a90dca3f
Loiphan1003 <phanvuloi001@gmail.com>
2023-09-25 18:36:36 +0700

    fix: ui for light theme

---
+++ stories/Tag.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Tag } from "../components/Tag";
+
+
+const meta: Meta<typeof Tag> = {
+    title: "Tag",
+    component: Tag,
+    tags: ["autodocs"],
+    parameters: {
+        layout: "centered",
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Demo: Story = {
+    args: {
+        label: "Demo",
+    },
+};
